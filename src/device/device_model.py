620a7fa23c3cf7419e9ce85af8317237ebdd535c
fatstas <stas9907092014@gmail.com>
2025-11-11 13:06:06 +0700

    Количество продувок, открытие и закрытие вытяжки при продувке, время подачи только при движении шнека

--- src/device/device_model.py
+++ src/device/device_model.py
@@ -61,7 +61,7 @@ class DeviceModelAuger:
         # подготовка очереди для непрерывного опроса
         if poller is not None:
             self._init_poller_queue()
-            # self.poller.init_func_calc_update_from_poller(self.update_from_poller)
+            self.poller.init_func_calc_update_from_poller(self.update_from_poller)
 
     def _init_poller_queue(self):
         # Подготавливаем очереди для данных
